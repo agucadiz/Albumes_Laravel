81fee767c7a03cf36ca6201683de44ec3fddb1ae
agucadiz <agustin.pedrote@iesdonana.org>
2023-02-02 22:12:01 +0100

    pre-albumes.index

--- resources/views/albumes/index.blade.php
+++ resources/views/albumes/index.blade.php
@@ -1,3 +1,11 @@
 <x-app-layout>
-    albumes.index
+    <x-slot name="header">
+        <h2 class="text-center font-semibold text-xl text-gray-800 leading-tight">
+            Álbumes
+        </h2>
+    </x-slot>
+    <div class="py-6">
+        <div class="p-6 bg-white border-gray-200 flex flex-col items-center max-w-7xl mx-auto px-10 shadow-sm sm:rounded-lg">
+            hola
+        </div>
 </x-app-layout>
